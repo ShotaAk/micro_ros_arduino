b30d4191b7dda5d568b3b5b768dabe41ec7b1927
ShotaAk <s.aoki@rt-net.jp>
2023-04-14 21:11:51 +0900

    Add picosensor msgs and esp32s3 library

---
+++ src/picosensor/msg/detail/light_sensor__struct.h
@@ -0,0 +1,43 @@
+// generated from rosidl_generator_c/resource/idl__struct.h.em
+// with input from picosensor:msg/LightSensor.idl
+// generated code does not contain a copyright notice
+
+#ifndef PICOSENSOR__MSG__DETAIL__LIGHT_SENSOR__STRUCT_H_
+#define PICOSENSOR__MSG__DETAIL__LIGHT_SENSOR__STRUCT_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+
+// Constants defined in the message
+
+/// Struct defined in msg/LightSensor in the package picosensor.
+typedef struct picosensor__msg__LightSensor
+{
+  int16_t forward_r;
+  int16_t forward_l;
+  int16_t left;
+  int16_t right;
+} picosensor__msg__LightSensor;
+
+// Struct for a sequence of picosensor__msg__LightSensor.
+typedef struct picosensor__msg__LightSensor__Sequence
+{
+  picosensor__msg__LightSensor * data;
+  /// The number of valid items in data
+  size_t size;
+  /// The number of allocated items in data
+  size_t capacity;
+} picosensor__msg__LightSensor__Sequence;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // PICOSENSOR__MSG__DETAIL__LIGHT_SENSOR__STRUCT_H_
